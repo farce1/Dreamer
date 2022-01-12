2351f367bf1a35fa216b5d472a75580fdb87a30c
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2022-01-12 21:45:32 +0100

    add meta

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -5,6 +5,13 @@ import '../styles/globals.css'
 export const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Dreamer</title>
+      <meta name="description" content="Dreamer landing page" />
+      <meta name="keywords" content="" />
+      <meta name="author" content="" />
       <title>Welcome to Dreamer!</title>
     </Head>
     <main>
